712b73230e1c104ec55e3e35a5f9f2f1e154d3dc
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-04 16:32:25 -0500

    logger with method, url, and time stamp completed

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const helmet = require('helmet');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
 const server = express();
+
+server.use(express.json(), logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +15,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const newDate = new Date(Date.now());
+  console.log((`${req.method} to ${req.originalUrl} at ${newDate.toDateString()}, ${newDate.toTimeString()}`))
+  next();
+};
 
 module.exports = server;
