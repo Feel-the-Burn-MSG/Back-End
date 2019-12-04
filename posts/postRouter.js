e11070a73f0206fc29068dc1831eb12f8209c1fb
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-04 18:39:20 -0500

    for postRoute just need to write in custom middleware and will be finished with that componenet

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -19,15 +19,82 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  postDb.getById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      console.log('error on GET /posts/:id', error);
+      res
+        .status(500)
+        .json({
+          message: 'The post information could not be retrieved.',
+        });
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  postDb.getById(id)
+    .then(post => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." })
+      } else {
+        postDb.remove(id)
+          .then(post => {
+            res
+              .status(201)
+              .json(post)
+          })
+          .catch(error => {
+            console.log('error on DELETE /posts/:id', error);
+            res
+              .status(500)
+              .json({ error: "The post could not be removed." });
+          });
+      };
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  const changes = req.body
+  console.log(req.body)
+
+  postDb.getById(id)
+    .then(post => {
+      console.log('post', post)
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." })
+      } else if (!changes.text) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide TEXT for the post." })
+      } else {
+        postDb.update(id, changes)
+          .then(post => {
+            res
+              .status(200)
+              .json(post)
+          })
+          .catch(error => {
+            console.log('error on PUT /posts/:id', error);
+            res
+              .status(500)
+              .json({ error: "The post information could not be modified." });
+          });
+      }
+    });
 });
 
 // custom middleware
