4b4be0a19e4841d81b8627eddc5e79439c679ee5
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-05 13:22:45 -0500

    added node start, and dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 
 const server = require('./server.js');
-
-server.listen(8000, () => {
-    console.log('\n* Server Running on http://localhost:8000 *\n');
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
